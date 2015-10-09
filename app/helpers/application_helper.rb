fe82ef1899e615acaebb150fb7e6bd06c84eb8cc
mitour <love2082020@gmail.com>
2015-10-09 15:53:55 +0000

    Rewrite navbar items with helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
 		title = "#{@title} | " + title if @title
 		title
 	end
+	def nav_li text, url
+		active = nil
+		active = :active if request.path == url
+		content_tag :li, class: "nav-item #{active}" do
+			link_to text, url
+		end
+	end
 end
