809e89d79e5ebfdd10806e317fbcb20695d7c847
mitour <love2082020@gmail.com>
2015-10-14 02:39:12 +0000

    Change routes setting of activities page: only keep index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  resources :activities
+  get  'activities' => 'activities#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
