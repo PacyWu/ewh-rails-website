1c1bd6533a2f7672ba0b1b4b2622c8868e69cfdb
mitour <love2082020@gmail.com>
2015-10-15 00:59:58 +0000

    Set routes of activities page: show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  get  'activities' => 'activities#index'
+  get  'activities'     => 'activities#index'
+  get  'activities/:id' => 'activities#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
