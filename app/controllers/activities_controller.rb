fd9fdcda57c6e08f109057ec2e0b45a8c604c35f
Yusheng Li <tamamali@icloud.com>
2015-10-25 09:24:05 +0000

    Setup routes and ActivitiesController to serve staticpages

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,8 @@
 class ActivitiesController < ApplicationController
     def index
     end
+
+    def show
+      render template: "pages/#{params[:page]}"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  resources :activities, only: [:index, :show]
+  resources :activities, only: :index do
+    collection do
+      get '/:page', action: 'show'
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
